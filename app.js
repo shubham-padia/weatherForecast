efccfc98d8ebf0b2bc3755e5740cae395e898e47
shubham-padia <shubhamapadia@gmail.com>
2016-08-13 23:51:23 +0530

    Seting up the app

--- app.js
+++ app.js
@@ -7,16 +7,24 @@ wspa.config(function($routeProvider){
         controller: 'mainController'
     })
     .when('/forecast',{
-        templateUrl: 'pages/main.html',
+        templateUrl: 'pages/forecast.html',
         controller: 'forecastController'
     })
 });
 
-wspa.controller('mainController', ['$scope', function ($scope) {
+wspa.service('nameService',function(){
+    this.city = "";
+});
 
+wspa.controller('mainController', ['$scope','nameService', function ($scope,nameService) {
+    $scope.cityName = nameService.city;
+    $scope.$watch('cityName',function(){
+          nameService.city = $scope.cityName;
+      });
 }]);
 
-wspa.controller('forecastController',['$scope', function ($scope) {
-
+wspa.controller('forecastController',['$scope','nameService', function ($scope,nameService) {
+    $scope.cityName = nameService.city; 
+    console.log($scope.cityName);
 }]);
 
